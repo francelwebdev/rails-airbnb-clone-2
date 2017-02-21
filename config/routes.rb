7e366d5e35a47a5051074160cb1ad222bde47b45
Cori Chong <corichong@gmail.com>
2017-02-21 12:15:24 +0000

    Added treehouses to index page

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   resources :treehouses, only: [:index, :show]
 
   devise_for :users
-  root to: 'pages#home'
+  root to: 'treehouses#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
