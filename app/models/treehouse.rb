5720b890e9b22226d16870bbc713e3831ebd2825
Cori Chong <corichong@gmail.com>
2017-02-22 08:29:53 +0000

    Front end updates

--- app/models/treehouse.rb
+++ app/models/treehouse.rb
@@ -1,6 +1,7 @@
 class Treehouse < ApplicationRecord
   belongs_to :owner, :class_name => 'User'
-  has_many :bookings
+  has_many :bookings, dependent: :destroy
+
   has_many :amenities
   has_many :spaces
   has_attachments :photos, maximum: 2
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :omniauthable, omniauth_providers: [:facebook]
-  has_many :bookings
+  has_many :bookings, dependent: :destroy
   has_many :treehouses
 
   def self.find_for_facebook_oauth(auth)
